528f11385b253fdaef619fdb0da36164e04ee262
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-14 10:00:27 -0700

    Add stlying to About Us and Terpenes tabs

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -6,8 +6,8 @@ const NavBar = () => {
     <nav className='nav-container'>
       <h1 className='title'>GET TERP'D</h1>
       <div className='nav-tabs-container'>
-        <h2>Terpenes</h2>      
-        <h2>About Us</h2>
+        <h2 className='terpenes'>Terpenes</h2>      
+        <h2 className='about'>About Us</h2>
       </div>
     </nav>
   )
