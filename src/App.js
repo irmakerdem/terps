360bc0264eef5095c7a653bd7e7f9ad472978e2b
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-14 08:41:51 -0700

    Create Routes component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
+import Routes from './Components/Routes/Routes';
 
 const App = () => {
   return (
     <main className="App">
       <NavBar />
+      <Routes />
       <Footer />
     </main>
   );
