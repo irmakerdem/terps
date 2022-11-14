4b7ce72695e2714323ce52a7d068658af0af9617
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-14 09:01:15 -0700

    Add styling to NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './NavBar.scss';
 
 const NavBar = () => {
   return (
-    <></>
+    <nav className='nav-container'>
+      <h1 className='title'>GET TERP'D</h1>
+      <div className='nav-tabs-container'>
+        <h2>Terpenes</h2>      
+        <h2>About Us</h2>
+      </div>
+    </nav>
   )
 }
 
