7598a317071439d17c57667aa4ec8c95528150fc
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-14 09:07:13 -0700

    Add styling to Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <></>
+    <footer className='footer-container'>
+      <p>Copyright (c) 2022</p>
+    </footer>
   )
 }
 
