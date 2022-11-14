33795d78113320b938dee1e5dfab3d75f83ed25a
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-14 15:49:15 -0700

    Adjust padding on Terpenes and About Us tabs

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -6,14 +6,14 @@ const Dropdown = () => {
     <div className='dropdown-container'>
       <h3 className='directions'>Select your favorite cultivar to view cultivars of similar terpene profiles.</h3>
       <form className='dropdown'>
-        <select>
+        <select className='dropdown-values'>
           <option value='choose'>🌱 Choose A Cultivar:</option>
           <option value='Blue Dream'>Blue Dream</option>
           <option value='Grandaddy Purple'>Grandaddy Purple</option>
           <option value='OG Kush'>OG Kush</option>
         </select>
         <br></br>
-        <button>GO!</button>
+        <button className='button'>GO!</button>
       </form>   
     </div>
   )
