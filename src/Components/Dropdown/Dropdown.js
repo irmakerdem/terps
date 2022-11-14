11573f993563adbbdbaa15fb075d7d4280810061
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-14 15:22:50 -0700

    Add dropdown, directions, and trichome image to homeview

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -3,18 +3,19 @@ import './Dropdown.scss';
 
 const Dropdown = () => {
   return (
-    <>
-      <p>Select your favorite cultivar to view cultivars of similar terpene profiles.</p>
-      <form>
+    <div className='dropdown-container'>
+      <h3 className='directions'>Select your favorite cultivar to view cultivars of similar terpene profiles.</h3>
+      <form className='dropdown'>
         <select>
           <option value='choose'>🌱 Choose A Cultivar:</option>
           <option value='Blue Dream'>Blue Dream</option>
           <option value='Grandaddy Purple'>Grandaddy Purple</option>
           <option value='OG Kush'>OG Kush</option>
         </select>
+        <br></br>
         <button>GO!</button>
       </form>   
-    </>
+    </div>
   )
 }
 
--- src/Views/HomeView/HomeView.js
+++ src/Views/HomeView/HomeView.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Dropdown from '../../Components/Dropdown/Dropdown';
+import trichomes from './../../assets/trichome-logo.png';
+import './HomeView.scss';
 
 const HomeView = () => {
   return (
-    <Dropdown />
+    <div className='homeview-container'>
+      <img src={trichomes} alt='trichomes illustration' className='trichomes'></img>
+      <Dropdown />
+    </div>
   )
 }
 
