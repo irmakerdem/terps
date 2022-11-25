2de0fa6ecc218c9dd44ab8bcb785f0a26e2149aa
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-25 15:38:09 -0700

    Add functionality to move between home, about, and terps pages

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NavBar.scss';
 
 const NavBar = () => {
   return (
     <nav className='nav-container'>
-      <h1 className='title'>GET TERP'D</h1>
+      <Link to='/'>
+        <button className='title'>GET TERP'D</button>
+      </Link>
       <div className='nav-tabs-container'>
-        <h2 className='terpenes'>Terpenes</h2>      
-        <h2 className='about'>About Us</h2>
+        <Link to='/terpenes'>
+          {/* <h2 className='terpenes'>Terpenes</h2>    */}
+          <button className='terpenes'>Terpenes</button>      
+        </Link>
+        <Link to='about'>
+          {/* <h2 className='about'>About Us</h2> */}
+          <button className='about'>About Us</button>
+        </Link>
       </div>
     </nav>
   )
