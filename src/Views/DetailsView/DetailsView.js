5ee99bccf370b110e1ffb7a2658d54935f389557
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-25 15:18:26 -0700

    Create DetailsView component

---
+++ src/Views/DetailsView/DetailsView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DetailsView.scss';
+
+const DetailsView = () => {
+  return (
+    <div className='details-container'>
+      <p>OG Kush</p>
+    </div>
+  )
+}
+
+export default DetailsView;
