e4f1926ae2194de0036d568233a5f5c7538326cb
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-25 16:22:47 -0700

    Remove hyperlink color

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -10,12 +10,12 @@ const NavBar = () => {
       </Link>
       <div className='nav-tabs-container'>
         <Link to='/terpenes'>
-          {/* <h2 className='terpenes'>Terpenes</h2>    */}
-          <button className='terpenes'>Terpenes</button>      
+          <h2 className='terpenes'>Terpenes</h2>   
+          {/* <button className='terpenes'>Terpenes</button>       */}
         </Link>
         <Link to='about'>
-          {/* <h2 className='about'>About Us</h2> */}
-          <button className='about'>About Us</button>
+          <h2 className='about'>About Us</h2>
+          {/* <button className='about'>About Us</button> */}
         </Link>
       </div>
     </nav>
