932efbddb03c6d95c1c2ffc25bc7e56e0acfb6d2
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-25 15:20:50 -0700

    Update TerpenesView statements

---
+++ src/Views/TerpenesView/TerpenesView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TerpenesView.scss';
+
+const TerpenesView = () => {
+  return (
+    <div className='terpenes-container'>
+      <p>What are terpenes?</p>
+      <p>Terpenes are natural compounds in plants responsible for their smells. They are not unique to cannabis.</p>
+      <p>Most common terpenes in cannabis are mycene, linalool, limonen, pinene, and caryophyllene.</p>
+      <p>Terpenes play a role in the Entourage Effect.</p>
+    </div>
+  )
+}
+
+export default TerpenesView;
