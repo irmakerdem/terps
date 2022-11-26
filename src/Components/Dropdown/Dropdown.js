68b33211815d92cf6fb4ee22e38dc0315ae5c0cc
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-25 17:27:14 -0700

    Center About and Terpenes pages

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Dropdown.scss';
+import { Link } from 'react-router-dom';
 
 const Dropdown = () => {
   return (
@@ -7,14 +8,17 @@ const Dropdown = () => {
       <h3 className='directions'>Select your favorite cultivar to view cultivars of similar terpene profiles.</h3>
       <form className='dropdown'>
         <select className='dropdown-values'>
-          <option value='choose'>🌱 Choose A Cultivar:</option>
+          <option value='choose'>🌱 Choose a Cultivar:</option>
           <option value='Blue Dream'>Blue Dream</option>
           <option value='Grandaddy Purple'>Grandaddy Purple</option>
           <option value='OG Kush'>OG Kush</option>
         </select>
         <br></br>
+        {/* <Link to={`/${id}`}>
+          <button className='button'>GO!</button>
+        </Link> */}
         <button className='button'>GO!</button>
-      </form>   
+      </form>
     </div>
   )
 }
