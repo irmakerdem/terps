d407ba90b6ea2236d488bb67ed278f96874f9b29
Irmak Erdem <irmakerdem9@gmail.com>
2022-12-19 12:53:05 -0700

    Add padding to Terps and About pages

--- src/Views/TerpenesView/TerpenesView.js
+++ src/Views/TerpenesView/TerpenesView.js
@@ -4,7 +4,7 @@ import './TerpenesView.scss';
 const TerpenesView = () => {
   return (
     <div className='terpenes-container'>
-      <p>What are terpenes?</p>
+      <h3>What are terpenes?</h3>
       <p>Terpenes are natural compounds in plants responsible for their smells. They are not unique to cannabis.</p>
       <p>Most common terpenes in cannabis are mycene, linalool, limonen, pinene, and caryophyllene.</p>
       <p>Terpenes play a role in the Entourage Effect.</p>
