7a1b2be5e2de91eb63d3f97cbfc5a6550eaad7a5
Irmak Erdem <irmakerdem9@gmail.com>
2023-01-17 18:54:14 -0700

    Create unordered list for similar strains

--- src/Components/StrainDetails/StrainDetails.js
+++ src/Components/StrainDetails/StrainDetails.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './StrainDetails.scss';
 
 const StrainDetails = ( {selectedStrain} ) => {
-  console.log(selectedStrain)
+  const effList = selectedStrain.effects.join(', ')
+  const simList = selectedStrain.similar.map((cult) => <li key={cult}>{cult}</li>);
+  
   return (
     <div className='details-container'>
-      <h1>{selectedStrain.strain}</h1>
-      <p>Terpenes: {selectedStrain.terpenes}</p>
-      <p>Effects: {selectedStrain.effects}</p>
-      <p>Similar Strain(s): {selectedStrain.similar}</p>
+      <h1 className='strain-name'>{selectedStrain.strain}</h1>
+      <div className='strain-details'>
+        <p>Terpenes: <br></br>{selectedStrain.terpenes}</p>
+        <p>Effects: <br></br>{effList}</p>
+        <p>Similar Strain(s):</p>
+          <ul>{simList}</ul>
+      </div>
     </div>
 )
   }
