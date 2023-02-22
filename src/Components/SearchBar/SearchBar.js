2fbab3abec70ed79767b0eccbdec7a5da0cd32f5
Irmak Erdem <irmakerdem9@gmail.com>
2023-02-22 14:42:59 -0700

    Add .trim() to input value to prevent user submitting empty spaces

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -7,7 +7,7 @@ const SearchBar = ( {searchStrain} ) => {
 
   const getInputValue = (event) => {
     // console.log(event)
-    setInputStrain(event.target.value)
+    setInputStrain(event.target.value.trim())
   }
 
   return (
@@ -15,9 +15,9 @@ const SearchBar = ( {searchStrain} ) => {
       {/* {console.log(matchingStrains)} */}
       <h3 className='directions'>Search your favorite cultivar to view cultivars of similar terpene profiles.</h3>
       <form>
-        <input name='search' type='text' placeholder='Search' required onChange={(event) => getInputValue(event)}></input>
+        <input name='search' type='text' placeholder='Search' onChange={(event) => getInputValue(event)}></input>
         <Link to='/searchResults' className='fake-button'>
-          <button disabled={!inputStrain.length} type='submit' className='go' onClick={() => {searchStrain(inputStrain)}}>GO</button>
+          <button disabled={!inputStrain.length || inputStrain === ' '} type='submit' className='go' onClick={() => {searchStrain(inputStrain)}}>GO</button>
         </Link>
       </form>
     </>
