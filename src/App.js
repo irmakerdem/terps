2d9d52fe0e0e3cc2d53c351e33dc9a16ac86fe00
Irmak Erdem <irmakerdem9@gmail.com>
2023-02-27 20:02:22 -0700

    Import Axios and perform GET call

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
 import Routes from './Components/Routes/Routes';
 import mockData from './mockData';
+import { getAllStrains } from './apiCalls';
 
 const App = () => {
 
   const [matchingStrains, setMatchingStrains] = useState([]);
 
+  useEffect(() => {
+    getAllStrains()
+    .then(responseee => {
+      console.log(responseee.data.data)
+      setMatchingStrains(responseee)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
   const searchStrain = (input) => {
     // console.log("input ➡️", input)
-    // if(!input) {
-    //   alert("Please enter a strain name! 🌱")
-    //   input = 'strain is empty!'
-    //   return
-    // }
     let foundStrains = mockData.filter((mock) => {
       // console.log("MOCKKKK➡️", mock)
       let name = mock.strain.toLowerCase();
       // console.log("name ➡️", name)
       if (name.includes(input.toLowerCase())) {
-        console.log("my mock➡️", mock)
+        // console.log("my mock➡️", mock)
         return mock;
       } else {
         return null;
       }
     })
-    console.log("foundStrains ➡️", foundStrains)
+    // console.log("foundStrains ➡️", foundStrains)
     let sortedArr = foundStrains.sort((a, b) => {
       return a.strain.localeCompare(b.strain)
     })
@@ -34,8 +39,8 @@ const App = () => {
   }
 
   const selectResult = (singleStrain) => {
-    console.log("matchingStrains ➡️", matchingStrains)
-    console.log("singleStrain ➡️", singleStrain)
+    // console.log("matchingStrains ➡️", matchingStrains)
+    // console.log("singleStrain ➡️", singleStrain)
     return matchingStrains.find(st => {
       return st.strain.replace(/ /g, "") === singleStrain.strainName
     })
@@ -44,6 +49,7 @@ const App = () => {
 
   return (
     <main className='App'>
+      {console.log("matchingStrains", matchingStrains)}
       <NavBar />
       <Routes searchStrain={searchStrain} matchingStrains={matchingStrains} selectResult={selectResult} />
       <Footer />
---
+++ src/apiCalls.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const getAllStrains = () => {
+  let url = `https://terp-backend-develop.onrender.com/api/strains`
+
+  let ourHeader = {Authorization: 'Bearer ac907442ffe1b2868c8c794867ae480ca47b51cbee003999c66058014aa5b894da8192363d65fa44e9d8c3a8368b0182cb1e8a19bb680e996efb37c045b323e05a31c9642af25d9e2d64aff0bd0278337c63591bb45be1728e84f16855fa347178b660188cb038011c893d0b94d3f38968faf34aba6b98eb31a55668578b49b8'}
+
+  let response = axios.get(url, {headers: ourHeader})
+
+  return response
+}
+
+//   //https://terp-backend-develop.onrender.com/api -> make into "rootURL"
+//   // let url = `https://terp-backend-develop.onrender.com/api/strains/?filters[name][$containsi]=${searchInput}`
+//   // let url = `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=xUoI1JmR6Bufafw2f3ZRJOQZUbAcO5L9`
+
+//   return fetch(url)
+//     .then(response => {
+//       if(!response.ok) {
+//         throw new Error('Oopsies! Something went wrong 🤡')
+//       } else {
+//         return response.json()
+//       }
+//     })
+
+
+// export default getAllStrains;
